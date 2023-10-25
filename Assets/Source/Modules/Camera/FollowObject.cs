b464cdbdb7f41f353736bbf1af1c815ca518268d
donl0 <donl_117n@mail.ru>
2023-10-25 21:38:21 +0300

    Add base camera follower

---
+++ Assets/Source/Modules/Camera/FollowObject.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class FollowObject : MonoBehaviour, IFlowControl
+{
+    [SerializeField] private Transform _target;
+    [SerializeField] private Vector3 _offset;
+
+    private bool _isActive = false;
+    
+    private void Update()
+    {
+        if (_isActive == false)
+            return;
+        
+        transform.position = _target.position + _offset;
+    }
+
+    public void Activate()
+    {
+        _isActive = true;
+    }
+
+    public void DeActivate()
+    {
+        _isActive = false;
+    }
+}
