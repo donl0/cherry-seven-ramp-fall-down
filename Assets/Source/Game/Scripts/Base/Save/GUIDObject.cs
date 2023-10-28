a0a41f6e5c03b5cbfaacd5dcf885d13d0f19f878
donl0 <donl_117n@mail.ru>
2023-10-28 21:49:48 +0300

    Add save load logic

---
+++ Assets/Source/Game/Scripts/Base/Save/GUIDObject.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+using System;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class GUIDObject : MonoBehaviour
+{
+#if UNITY_EDITOR
+    //[ReadOnly]
+#endif
+    [SerializeField] private string _guid;
+
+    public string GUID => _guid;
+
+#if UNITY_EDITOR
+    [ContextMenu("Regenerate GUID")]
+    public void RegenerateGUID()
+    {
+        _guid = Guid.NewGuid().ToString();
+        EditorUtility.SetDirty(gameObject);
+    }
+#endif
+}
