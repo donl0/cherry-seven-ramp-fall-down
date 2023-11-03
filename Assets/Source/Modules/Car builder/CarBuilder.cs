a3559ca6372d37ab1aa705f6c231ecaef62bab39
donl0 <donl_117n@mail.ru>
2023-11-04 01:06:06 +0300

    Add car builder

---
+++ Assets/Source/Modules/Car builder/CarBuilder.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CarBuilder: MonoBehaviour , ICarBuilder
+{
+    [SerializeField] private Transform _container;
+    
+    [SerializeField] private CurrentCarHandler _currentCar;
+    [SerializeField] private CarPrefabs _typesPrefabs;
+
+    public GameObject Build(Vector3 position)
+    {
+        _typesPrefabs.TryGetPrefab(_currentCar.Load(), out GameObject prefab);
+        var spawned = Instantiate(prefab, position, Quaternion.identity, _container);
+
+        return spawned;
+    }
+}
---
+++ Assets/Source/Modules/Car builder/ICarBuilder.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public interface ICarBuilder
+{
+    public GameObject Build(Vector3 position);
+}
