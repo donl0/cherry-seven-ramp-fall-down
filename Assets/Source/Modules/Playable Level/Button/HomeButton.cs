7dc7eb208d9534d29ffdfaf73b6c6da867d4e2d6
donl0 <donl_117n@mail.ru>
2023-11-16 01:44:26 +0300

    Add Circles with GameObject view and place to Stair scene

---
+++ Assets/Source/Modules/Playable Level/Button/HomeButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine.Events;
+
+public class HomeButton : ButtonConnector, IHomeClickable
+{
+    public UnityAction HomeButtonClicked { get; set; }
+
+    protected override void OnClicked()
+    {
+        HomeButtonClicked?.Invoke();
+    }
+}
