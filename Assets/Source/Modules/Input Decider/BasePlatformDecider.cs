863a74c756797b24b84477be205dbe0387ffaa4a
donl0 <donl_117n@mail.ru>
2023-11-17 12:19:50 +0300

    Add graphy

--- Assets/Source/Modules/Input Decider/BasePlatformDecider.cs
+++ Assets/Source/Modules/Input Decider/BasePlatformDecider.cs
@@ -1,9 +1,23 @@
-﻿using UnityEngine;
+﻿using Agava.YandexGames;
+using UnityEngine;
+using UnityEngine.iOS;
 
 internal class BasePlatformDecider:IPlatformDecider
 {
     public Platform Take()
     {
+        if (Application.platform == RuntimePlatform.Android)
+            return Platform.Mobile;
+        else
+            return Platform.PC;
+    }
+}
+
+internal class YandexPlatformDecider:IPlatformDecider
+{
+    public Platform Take()
+    {
+        Device.IsMobile 
         if (Application.platform == RuntimePlatform.Android)
             return Platform.Mobile;
         else
