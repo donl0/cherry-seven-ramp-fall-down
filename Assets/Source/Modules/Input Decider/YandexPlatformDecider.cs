b8b146dc2f7177c4447389e1a6d0b8beaab5192a
donl0 <donl_117n@mail.ru>
2023-11-17 12:34:31 +0300

    Update Input for Yandex

---
+++ Assets/Source/Modules/Input Decider/YandexPlatformDecider.cs
@@ -0,0 +1,10 @@
+﻿internal class YandexPlatformDecider: IPlatformDecider
+{
+    public Platform Take()
+    {
+        if (Device.IsMobile == true)
+            return Platform.Mobile;
+        else
+            return Platform.PC;
+    }
+}
